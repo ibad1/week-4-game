a6875130eccd283d969971f14e142c30fe274e9a
Ibad Aslam <Ibad Aslam>
2017-02-12 04:04:03 -0500

    updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,60 @@
-
-var wins = 0;
-var losses = 0;
-var score = 0;
-var crystalValue = [];
-var btn = 0;
-
-
-
-$(document).ready(function() {
-
+var score;
+var min=19;
+var max=120;
+var win=0;
+var lose=0;
+var total=0;
+var crystals = [];
+var random = Math.floor(Math.random()*(max - min + 1)) + min;
+$("#myNumber").html(random);
 
 
-	function randNum(min, max) {
-		return Math.floor(Math.random() * (max - min) + min);
-	}	
-
-
-	var target = randNum(19, 120);
-	
-	$('#myNumber').append(target);
-
-
-	for (var i = 0; i <= 3; i++) {
-		crystalValue[i] = randNum(1, 12);
-		
-	}	
-
-
-	$('#btn1').click(function(result) {
-		var btn = crystalValue[0];
-		score += btn;
-		$('#currentScore').html(score);
-		
-		
-		
-	});
-
-
-	$('#btn2').click(function(result) {
-		var btn = crystalValue[1];
-		score += btn;
-		$('#currentScore').html(score);
-	
-		
-		
-	});
-
-	$('#btn3').click(function(result) {
-		var btn = crystalValue[2];
-		score += btn;
-		$('#currentScore').html(score);
-
-
-	});
-
-
-	$('#btn4').click(function(result) {
-		var btn = crystalValue[3];
-		score += btn;
-		$('#currentScore').html(score);
-
-		
-
-	});
-	console.log(target);
-	console.log(score);
-	console.log (crystalValue);
-
-	var test = parseInt($('#currentScore'))
-	console.log (test);
-	if (target == score) {
-		wins++
-		$("#winCount").append(wins);
+for (var i=0; i<4; i++){
+	crystals[i]={
+		points:10
 	}
-	else if (target < score){
-		losses++
-		$("#lossCount").append(losses);
+	AssignValue(crystals[i])
+}
+
+var count=0;
+$(".buttons").each(function(){
+	var btn= $(this);
+	btn.attr("data-crystal", count);
+	btn.on("click", function()
+		{
+			var clickedBtn = $(this);
+			var crystalIndex = clickedBtn.attr("data-crystal");
+			total=total+crystals[crystalIndex].points;
+			$("#currentScore").html(total);
+			if (total === random){
+			win++;
+			$("#winCount").html(win);
+			ResetGame();
+			}
+			else if (total>random) {
+			lose++
+			$("#lossCount").html(lose);
+			ResetGame();
+			}
+
+		});
+	count++;
+});
+
+function AssignValue(crystal){
+	var min=1;
+	var max=12;
+	crystal.points = Math.floor(Math.random()*(max-min+1))+min;
+
+}
+function ResetGame(){
+	total=0;
+	$("#addpoints").html(total);
+	random=Math.floor(Math.random()*(max-min+1))+min;
+	$("#randomnumber").html(random);
+	for (var i=0; i<crystals.length; i++)
+	{
+		AssignValue(crystals[i])
 	}
 
-
-}); 
-
+}
